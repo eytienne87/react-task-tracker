0bd1aace8f9bf2e19fe1f88f9b257f3d7885701c
Etienne Tessier <etienne_tessier100@hotmail.ca>
2021-11-12 19:08:31 -0500

    Implemented toggle of the form to add a task

--- src/App.js
+++ src/App.js
@@ -45,8 +45,9 @@ const App = () => {
 
   return (
     <div className='container'>
-        <Header/>
-        {showAddTask && <AddTask onAdd={addTask}/>}
+      <Header onAdd={() => setShowAddTask(!showAddTask)}
+      showAdd={showAddTask}/>
+        {showAddTask && <AddTask onAdd={addTask} />}
         {tasks.length > 0 ? <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} /> : 'No Tasks to show' }
     </div>
   );
